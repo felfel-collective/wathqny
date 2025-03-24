ee73fe82cd40ca80d6aee35a2020b139df49242d
abdo-spices <spices.digital@gmail.com>
2025-03-24 14:17:36 +0200

    refactor(astro): update astro content layer 🎉

--- packages/wathqny/schema.ts
+++ packages/wathqny/schema.ts
@@ -1,13 +1,20 @@
 import { defineCollection } from 'astro:content'
 import { blogSchema, docSchema } from 'astro-toolkit/schema'
+import { glob } from 'astro/loaders';
 
 const docs = defineCollection({
-  type: 'content',
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "src/content/docs"
+  }),
   schema: docSchema,
 })
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "src/content/blog"
+  }),
   schema: blogSchema,
 })
 

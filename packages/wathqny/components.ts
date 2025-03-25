4159a1f71a69c3a622b688c3669957b7a9d716f8
abdo-spices <spices.digital@gmail.com>
2025-03-25 06:32:36 +0200

    fix(build): unavailable imports change astro auto import path

---
+++ packages/wathqny/components.ts
@@ -0,0 +1,4 @@
+export { default as Callout } from './src/components/common/Callout.astro'
+export { default as Code } from './src/components/Block/Code.astro'
+export type { Props as CodeType } from './src/components/Block/Code.astro'
+export { default as BrowserBlock } from './src/components/Block/BrowserBlock.astro'

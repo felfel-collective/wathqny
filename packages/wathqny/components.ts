9c7aa4c8e0f7196188cc8209169fa7c3f66ece64
abdo-spices <spices.digital@gmail.com>
2025-03-25 10:07:34 +0200

    refactor(wathqny): move all components to components folder for theme provider compatibly

--- packages/wathqny/components.ts
+++ packages/wathqny/components.ts
@@ -1,4 +1,4 @@
-export { default as Callout } from './src/components/common/Callout.astro'
-export { default as Code } from './src/components/Block/Code.astro'
-export type { Props as CodeType } from './src/components/Block/Code.astro'
-export { default as BrowserBlock } from './src/components/Block/BrowserBlock.astro'
+export { default as Callout } from './src/components/Callout.astro'
+export { default as Code } from './src/components/Code.astro'
+export type { Props as CodeType } from './src/components/Code.astro'
+export { default as BrowserBlock } from './src/components/BrowserBlock.astro'

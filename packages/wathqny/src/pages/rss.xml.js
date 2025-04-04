d6ec1961e7f3c3f76e5e7b633051263ae2d8758c
abdo-spices <spices.digital@gmail.com>
2025-04-04 19:44:50 +0200

    refactor(pages): clean up unused imports and streamline component imports in various files

--- packages/wathqny/src/pages/rss.xml.js
+++ packages/wathqny/src/pages/rss.xml.js
@@ -1,17 +1,18 @@
-// import rss from '@astrojs/rss'
-// import { getCollection } from 'astro:content'
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+import config from 'wathqny:config'
 
-// export async function GET(context) {
-//   const blog = await getCollection('blog')
-//   return rss({
-//     title: .siteName,
-//     description: .description,
-//     site: context.site,
-//     items: blog.map((post) => ({
-//       title: post.data.title,
-//       description: post.data.description,
-//       pubDate: post.data.pubDate,
-//       link: `/blog/${post.data.link}`,
-//     })),
-//   })
-// }
+export async function GET(context) {
+  const blog = await getCollection('blog')
+  return rss({
+    title: config.siteName,
+    description: config.description,
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.data.link}`,
+    })),
+  })
+}

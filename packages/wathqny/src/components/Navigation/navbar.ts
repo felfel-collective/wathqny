f2c0218ab33aebd5b0849c4f98d3199065b3222d
abdo-spices <spices.digital@gmail.com>
2025-07-21 08:05:02 +0300

    fix: simplify theme toggle logic and navbar activation

--- packages/wathqny/src/components/Navigation/navbar.ts
+++ packages/wathqny/src/components/Navigation/navbar.ts
@@ -1,23 +1,16 @@
 document.querySelector('.theme-switcher')?.addEventListener('click', () => {
   const root = document.documentElement
-  if (root?.classList.contains('dark')) {
-    root?.classList.replace('dark', 'light')
-    localStorage.setItem('theme', 'light')
-  } else {
-    root?.classList.replace('light', 'dark')
-    localStorage.setItem('theme', 'dark')
-  }
+
+  root?.classList.toggle('dark', root?.classList.contains('dark'))
+  localStorage.setItem('theme', 'dark')
 })
 
 const navbarToggle = document.querySelector('.wtq-toggle')
 const navbar = document.querySelector('.wtq-links')
 
 navbarToggle?.addEventListener('click', () => {
-  if (navbar?.classList.contains('wtq-active-links')) {
-    navbar?.classList.remove('wtq-active-links')
-    document.body.style.overflow = ''
-  } else {
-    navbar?.classList.add('wtq-active-links')
-    document.body.style.overflow = 'hidden'
-  }
+  const isActive = navbar?.classList.contains('wtq-active-links')
+  
+  navbar?.classList.toggle('wtq-active-links', isActive)
+  document.body.style.overflow = isActive ? 'hidden' : ''
 })

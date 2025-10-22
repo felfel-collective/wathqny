ae5b88ae96393ce32b267eebbb39f6a50c5f9bd5
abdo.spices <spices.digital@gmail.com>
2025-10-22 05:06:17 +0300

    fix: dark mode theme

--- packages/wathqny/src/components/Navigation/navbar.ts
+++ packages/wathqny/src/components/Navigation/navbar.ts
@@ -1,9 +1,11 @@
 document.querySelector('.theme-switcher')?.addEventListener('click', () => {
-  const root = document.documentElement
-
-  root?.classList.toggle('dark', !root?.classList.contains('dark'))
-  localStorage.setItem('theme', 'dark')
-})
+  const root = document.documentElement;
+  const isDark = root.classList.contains('dark');
+  
+  root.classList.toggle('dark', !isDark);
+  root.classList.toggle('light', isDark);
+  localStorage.theme = isDark ? 'light' : 'dark';
+});
 
 const navbarToggle = document.querySelector('.wtq-toggle')
 const navbar = document.querySelector('.wtq-links')
